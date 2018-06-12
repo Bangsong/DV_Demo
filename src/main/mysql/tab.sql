d37f5074705eb82d7a3190da99b5ce66696ee249
rzp77889 <1074720760@qq.com>
2018-06-12 18:54:42 +0800

    动态生成echarts图表调整

---
+++ src/main/mysql/tab.sql
@@ -0,0 +1,243 @@
+/*
+SQLyog Ultimate v12.3.1 (64 bit)
+MySQL - 8.0.11 : Database - dv
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dv` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `dv`;
+
+/*Table structure for table `company` */
+
+DROP TABLE IF EXISTS `company`;
+
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '公司ID',
+  `name` varchar(100) NOT NULL COMMENT '公司全名',
+  `alias` varchar(10) NOT NULL COMMENT '公司别名',
+  `address` varchar(1000) NOT NULL COMMENT '公司地址',
+  `Province` varchar(20) NOT NULL COMMENT '所属省',
+  `City` varchar(20) NOT NULL COMMENT '所属市',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `company` */
+
+insert  into `company`(`id`,`name`,`alias`,`address`,`Province`,`City`) values 
+(1,'万能的小明工作室','XGame','河南省郑州市','河南','郑州');
+
+/*Table structure for table `department` */
+
+DROP TABLE IF EXISTS `department`;
+
+CREATE TABLE `department` (
+  `corp_id` int(11) NOT NULL COMMENT '公司ID',
+  `id` int(11) NOT NULL COMMENT '部门ID',
+  `name` varchar(100) NOT NULL COMMENT '部门名',
+  `sub_depart_id` int(11) NOT NULL DEFAULT '0' COMMENT '所属部门ID',
+  `job_id` int(11) NOT NULL COMMENT '职位ID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `department` */
+
+insert  into `department`(`corp_id`,`id`,`name`,`sub_depart_id`,`job_id`) values 
+(1,1,'技术部',0,1);
+
+/*Table structure for table `e_legend` */
+
+DROP TABLE IF EXISTS `e_legend`;
+
+CREATE TABLE `e_legend` (
+  `corp_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `type` varchar(50) NOT NULL DEFAULT 'plain',
+  `show` tinyint(1) NOT NULL DEFAULT '0',
+  `left` varchar(50) NOT NULL DEFAULT 'auto',
+  `top` varchar(50) NOT NULL DEFAULT 'auto',
+  `right` varchar(50) NOT NULL DEFAULT 'auto',
+  `bottom` varchar(50) NOT NULL DEFAULT 'auto',
+  `width` varchar(10) NOT NULL DEFAULT 'auto',
+  `height` varchar(10) NOT NULL DEFAULT 'auto',
+  `data` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `e_legend` */
+
+/*Table structure for table `e_series` */
+
+DROP TABLE IF EXISTS `e_series`;
+
+CREATE TABLE `e_series` (
+  `corp_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `data` varchar(1000) NOT NULL,
+  `smooth` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `e_series` */
+
+insert  into `e_series`(`corp_id`,`id`,`data`,`smooth`) values 
+(1,1,'820, 932, 901, 934, 1290, 1330, 1320\r\n',1);
+
+/*Table structure for table `e_title` */
+
+DROP TABLE IF EXISTS `e_title`;
+
+CREATE TABLE `e_title` (
+  `corp_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `show` tinyint(1) NOT NULL DEFAULT '1',
+  `text` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `link` varchar(100) DEFAULT NULL,
+  `target` varchar(50) NOT NULL DEFAULT 'blank',
+  `subtext` varchar(500) DEFAULT NULL,
+  `sublink` varchar(100) DEFAULT NULL,
+  `subtarget` varchar(50) NOT NULL DEFAULT 'blank',
+  `padding` varchar(80) DEFAULT NULL,
+  `itemGap` int(11) DEFAULT NULL,
+  `left` varchar(50) NOT NULL DEFAULT 'auto',
+  `top` varchar(50) NOT NULL DEFAULT 'auto',
+  `right` varchar(50) NOT NULL DEFAULT 'auto',
+  `bottom` varchar(50) NOT NULL DEFAULT 'auto',
+  `backgroundColor` varchar(100) NOT NULL DEFAULT 'transparent',
+  KEY `corp_id` (`corp_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `e_title` */
+
+insert  into `e_title`(`corp_id`,`id`,`show`,`text`,`link`,`target`,`subtext`,`sublink`,`subtarget`,`padding`,`itemGap`,`left`,`top`,`right`,`bottom`,`backgroundColor`) values 
+(1,1,1,'测试','https://www.baidu.com/','blank','测试副标题','http://echarts.baidu.com/','blank','0',10,'auto','auto','auto','auto','#000');
+
+/*Table structure for table `e_xaxis` */
+
+DROP TABLE IF EXISTS `e_xaxis`;
+
+CREATE TABLE `e_xaxis` (
+  `corp_id` int(11) NOT NULL COMMENT '公司ID',
+  `id` int(11) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `data` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `e_xaxis` */
+
+insert  into `e_xaxis`(`corp_id`,`id`,`type`,`data`) values 
+(1,1,'category','Mon,Tue,Wed,Thu,Fri,Sat,Sun');
+
+/*Table structure for table `e_yaxis` */
+
+DROP TABLE IF EXISTS `e_yaxis`;
+
+CREATE TABLE `e_yaxis` (
+  `CORP_ID` int(11) DEFAULT NULL,
+  `id` int(11) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `e_yaxis` */
+
+insert  into `e_yaxis`(`CORP_ID`,`id`,`type`) values 
+(1,1,'value');
+
+/*Table structure for table `echarts` */
+
+DROP TABLE IF EXISTS `echarts`;
+
+CREATE TABLE `echarts` (
+  `corp_id` int(11) NOT NULL COMMENT '公司ID',
+  `type` varchar(50) NOT NULL COMMENT '类型',
+  `t_id` int(11) NOT NULL DEFAULT '0' COMMENT '标题ID',
+  `x_id` int(11) NOT NULL DEFAULT '0' COMMENT 'xAxis ID',
+  `y_id` int(11) NOT NULL DEFAULT '0' COMMENT 'yAxis ID',
+  `s_id` int(11) NOT NULL DEFAULT '0' COMMENT 'series ID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `echarts` */
+
+insert  into `echarts`(`corp_id`,`type`,`t_id`,`x_id`,`y_id`,`s_id`) values 
+(1,'line',1,1,1,1);
+
+/*Table structure for table `jurisdicte` */
+
+DROP TABLE IF EXISTS `jurisdicte`;
+
+CREATE TABLE `jurisdicte` (
+  `corp_id` int(11) NOT NULL COMMENT '公司ID',
+  `id` int(11) NOT NULL COMMENT '职位ID',
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '职位名称',
+  `power_id` int(11) NOT NULL COMMENT '功能ID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `jurisdicte` */
+
+insert  into `jurisdicte`(`corp_id`,`id`,`name`,`power_id`) values 
+(1,1,'开发人员',1),
+(1,1,'开发人员',11),
+(1,1,'开发人员',10),
+(1,1,'开发人员',2),
+(1,1,'开发人员',20),
+(1,1,'开发人员',21),
+(1,1,'开发人员',22);
+
+/*Table structure for table `tb_power` */
+
+DROP TABLE IF EXISTS `tb_power`;
+
+CREATE TABLE `tb_power` (
+  `id` int(11) NOT NULL COMMENT '功能ID',
+  `name` varchar(100) NOT NULL COMMENT '功能名称',
+  `p_id` int(11) NOT NULL DEFAULT '0' COMMENT '父功能，0：无',
+  `url` varchar(200) NOT NULL COMMENT '请求地址',
+  `is_show` tinyint(1) NOT NULL DEFAULT '1' COMMENT '是否显示，1：显示，0：隐藏',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `tb_power` */
+
+insert  into `tb_power`(`id`,`name`,`p_id`,`url`,`is_show`) values 
+(1,'系统管理',0,'#',1),
+(2,'数据可视化',0,'#',1),
+(10,'用户管理',1,'/system/userSys',1),
+(11,'参数管理',1,'/system/paramSys',1),
+(20,'柱形报表',2,'#',1),
+(21,'折线报表',2,'#',1),
+(22,'echarts演示',2,'/DataGraph/demo',1);
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` varchar(100) NOT NULL COMMENT '用户账号',
+  `user_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户姓名',
+  `user_pwd` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户密码',
+  `user_hp` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户头像',
+  `sex` varchar(10) NOT NULL DEFAULT '男' COMMENT '性别',
+  `age` int(11) NOT NULL COMMENT '年龄',
+  `birthday` date NOT NULL COMMENT '生日',
+  `tel` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '联系电话',
+  `login_status` int(11) NOT NULL DEFAULT '0' COMMENT '登录状态：0：离线，1：在线',
+  `corp_id` int(11) NOT NULL COMMENT '公司ID',
+  `depa_id` int(11) NOT NULL COMMENT '部门ID',
+  `lock_flag` int(11) NOT NULL DEFAULT '0' COMMENT '锁定标识：0：解锁，1：锁定，即不能登录',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`user_id`,`user_name`,`user_pwd`,`user_hp`,`sex`,`age`,`birthday`,`tel`,`login_status`,`corp_id`,`depa_id`,`lock_flag`) values 
+('rzp','万能的小明','mima','/content/images/user.jpg','男',24,'1994-09-23','18237120593',1,1,1,0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- src/main/webapp/WEB-INF/content/js/DataGraph.js
+++ src/main/webapp/WEB-INF/content/js/DataGraph.js
@@ -9,18 +9,48 @@ function demo() {
         "corp_id":1,
         "type":"line"
     }
-    $.getJSON("/DataGraph/echartSet",$.param(params),function (dataS) {
-        $.each(dataS,function (i,item) {
+    $.getJSON("/DataGraph/echartSet",$.param(params),function (data) {
+        $.each(data,function (i,item) {
+            var option = {};
+            item.title_text !=""?option.title = {text:item.title_text}:0;
+            item.title_link !=""?option.title.link = item.title_link:0;
+            item.title_target !=""?option.target = item.title_target:0;
+            item.title_subtext !=""?option.title.subtext = item.title_subtext:0;
+            item.title_sublink !=""?option.title.sublink = item.title_sublink:0;
+            item.title_subtarget !=""?option.title.subtarget = item.title_subtarget:0;
+
+            item.title_padding !=""?option.title.padding = item.title_padding:0;
+            item.title_itemgap !=""?option.title.itemGap = item.title_itemgap:0;
+            item.title_left !=""?option.title.left = item.title_left:0;
+            item.title_top !=""?option.title.top = item.title_top:0;
+            item.title_right !=""?option.title.right = item.title_right:0;
+            item.title_bottom !=""?option.title.bottom = item.title_bottom:0;
+            item.backgroundcolor !=""?option.title.backgroundColor = item.backgroundcolor:0;
+
+            switch(item.e_type){
+                case "line":
+                    break;
+                case "bar":
+                    break;
+                case "pie":
+                    break;
+                case "scatter":
+                    break;
+                case "map":
+                    break;
+                case "candlestick":
+                    break;
+                case "radar":
+                    break;
+            };
             var xAxisData = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
             var seriesData = [820, 932, 901, 934, 1290, 1330, 1320];
-            var option ={};
-            item.text !=""?option.title = {text:item.text}:0;
             var x_dataArr = item.x_data.split(',');
             item.x_type !="" && x_dataArr.length > 0?option.xAxis = {type:item.x_type,data:x_dataArr}:0;
             item.y_type !=""?option.yAxis = {type:item.y_type}:0;
             var s_dataArr = item.s_data.split(',');
-            item.s_tpye !="" && s_dataArr.length > 0?option.series = {type:$.trim(item.s_tpye),data:s_dataArr}:0;
-
+            item.e_type !="" && s_dataArr.length > 0?option.series = {type:$.trim(item.e_type),data:s_dataArr}:0;
+            item.series_smooth !=""?option.series.smooth = item.series_smooth:0;
             // option = {
             //     title:{
             //         text:"测试"
