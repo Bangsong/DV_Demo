15673859940b97fc8ec516a1d2ab24379f50fefb
rzp77889 <1074720760@qq.com>
2018-06-23 18:10:15 +0800

    切换数据源添加

--- src/main/java/com/DV/service/dataGraphService.java
+++ src/main/java/com/DV/service/dataGraphService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface dataGraphService {
+    @DataSource("Echarts")
     List<Map> echartSet(String corp_id, String type);
     @DataSource("Echarts")
     List<Map> test();
